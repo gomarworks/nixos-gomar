25fbca577e185b1272966d9988a2459782dee25b
thibaultvdb <tb_vdb@hotmail.com>
2024-08-28 11:40:41 +0200

    kitty terminal change

--- home.nix
+++ home.nix
@@ -52,7 +52,9 @@
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
-    colorschemes.gruvbox.enable = true;
+    colorschemes.gruvbox = {
+      enable = true;
+    };
     plugins = {
       lualine.enable = true;
       telescope.enable = true;
@@ -72,7 +74,7 @@
     settings = {
       dynamic_background_opacity = true;
       confirm_os_window_close = 0;
-      #window_padding_width = 10;
+      window_padding_width = 0;
       background_opacity = "0.5";
       background_blur = 5;
     };
