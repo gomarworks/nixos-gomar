a31ba4a70d5c19140abbac2417097a59c9d8920a
thibaultvdb <tb_vdb@hotmail.com>
2024-09-08 01:06:52 +0200

    latesttryforwall

--- homeManagerModules/kitty.nix
+++ homeManagerModules/kitty.nix
@@ -7,7 +7,7 @@
       dynamic_background_opacity = true;  # Enable dynamic opacity
       confirm_os_window_close = 0;        # Disable close confirmation
       window_padding_width = 0;           # Set window padding to 0
-      background_opacity = "0.7";         # Set background opacity to 70%
+      # background_opacity = "0.7";         # Set background opacity to 70%
     };
   };
 }
