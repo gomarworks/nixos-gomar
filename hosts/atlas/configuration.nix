14b19a2e091fd035a1d9096b601828484b4ac1ac
thibaultvdb <tb_vdb@hotmail.com>
2024-09-25 16:03:09 +0200

    atlas boot update

--- hosts/atlas/configuration.nix
+++ hosts/atlas/configuration.nix
@@ -10,8 +10,17 @@
   #######################################################################
   # Bootloader Configuration workDesktop
   #######################################################################
-  boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.configurationLimit = 5;
+
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   boot.plymouth.enable = true;
   boot.initrd.verbose = false;
